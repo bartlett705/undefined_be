0d726522b097a4b6bb0d1e610d669a8c71e5e3a9
Ahmad Kanawi <ahmad@mosey.systems>
2018-11-26 21:47:20 -0800

    captures hidden files

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@
 rm -rf ~/temp_backup
 mkdir ~/temp_backup
 
-tar -czf ~/temp_backup/config.tar.gz /etc/nginx/nginx.conf /etc/letsencrypt/ ~/.ssh/*.* ~/src/undefined_be/.env
+tar -czf ~/temp_backup/config.tar.gz /etc/nginx/nginx.conf /etc/letsencrypt/ ~/.ssh/. ~/src/undefined_be/.env
 aws s3 cp ./temp_backup/ s3://mosey.systems/droplet_backup --recursive --metadata type=backup
