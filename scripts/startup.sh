e3919968ebf03e1f3da95f341add7535daee126d
Ahmad Kanawi <ahmad@mosey.systems>
2019-02-03 03:51:33 -0800

    updates for font of wisdom

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,5 +3,6 @@ tmux \
   new-session -s backends "cd ~/src/undefined_be ; npm start ; read" \; \
   split-window "cd ~/src/undefined_be ; npm run start:production ; read" \; \
   split-window "cd /var/www/hams-near-me/server ; npm run start:production ; read" \; \
+  split-window "cd ~/src/font-of-wisdom ; npm run start:production ; read" \; \
   select-layout even-vertical \; \
   detach
