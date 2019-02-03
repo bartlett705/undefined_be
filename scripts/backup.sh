1fdf86790348068445786fda08af417784d2ca66
Ahmad Kanawi <ahmad@mosey.systems>
2019-02-03 11:02:24 +0000

    script/nginx fixes

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,5 +5,5 @@ mkdir ~/temp_backup
 
 # needs sudo :/
 #tar -czf ~/temp_backup/config.tar.gz /etc/nginx/nginx.conf /etc/letsencrypt/ ~/.ssh/. ~/src/undefined_be/.env /var/www/undefined/src/config.ts
-tar -czf ~/temp_backup/config.tar.gz  ~/.ssh/. ~/src/undefined_be/.env /var/www/undefined/src/config.ts
-aws s3 cp ./temp_backup/ s3://mosey.systems/beloved_darkorange__backup --recursive --metadata type=backup
+tar -czf ~/temp_backup/config.tar.gz  ~/.ssh/. ~/src/undefined_be/.env /var/www/undefined/src/config.ts ~/.aws
+aws s3 cp ~/temp_backup/ s3://mosey.systems/beloved_darkorange__backup --recursive --metadata type=backup
