2d97c89bb37ee297315f2cdf17e3c164e8a6260e
Ahmad Kanawi <ahmad@mosey.systems>
2019-02-10 22:37:38 -0800

    weather polish

--- src/weather.ts
+++ src/weather.ts
@@ -1,10 +1,26 @@
 import fetch from 'node-fetch'
 import { config } from './config'
 import { Logger } from './logger'
-import { CLIResponseType } from './models/cli'
+import { CLIResponse, CLIResponseType } from './models/cli'
 import { generateDefaultError } from './responseUtils/generateDefaultError'
 
-export async function getWeather(logger: Logger, zip: string) {
+export async function getWeather(
+  logger: Logger,
+  zipString: string
+): Promise<CLIResponse> {
+  const zip = Number(zipString)
+
+  if (Number.isNaN(zip) || zip < 10000 || zip > 99999) {
+    return {
+      content: [
+        "That doesn't look like a zip code...",
+        'Usage: weather [5-digit zip]',
+        'ex: weather 92672 '
+      ],
+      type: CLIResponseType.Standard
+    }
+  }
+
   try {
     const res = await fetch(
       `https://api.apixu.com/v1/forecast.json?key=${
@@ -15,23 +31,24 @@ export async function getWeather(logger: Logger, zip: string) {
     const body = await res.json()
 
     logger.debug('Got a weather response:', res.status)
-
-    const { location, current, forecast } = body
-    const { day, astro } = forecast.forecastday[0]
-    const content = [
-      `${location.name} | ${location.lat} : ${location.lon} | ${
-        location.localtime
-      }  `,
-      `NOW: ${current.temp_f} F | clouds: ${current.cloud} | Feels like: ${
-        current.feelslike_f
-      } F `,
-      `TMRW: l ${day.mintemp_f} F | h: ${day.maxtemp_f} F | SR: ${
-        astro.sunrise
-      } | SS: ${astro.sunset}  `
-    ]
-    return { content, type: CLIResponseType.Standard }
+    if (res.status < 400) {
+      const { location, current, forecast } = body
+      const { day, astro } = forecast.forecastday[0]
+      const content = [
+        `${location.name} | ${location.lat} : ${location.lon} | ${
+          location.localtime
+        }  `,
+        `NOW: ${current.temp_f} F | clouds: ${current.cloud} | Feels like: ${
+          current.feelslike_f
+        } F `,
+        `TMRW: l ${day.mintemp_f} F | h: ${day.maxtemp_f} F | SR: ${
+          astro.sunrise
+        } | SS: ${astro.sunset}  `
+      ]
+      return { content, type: CLIResponseType.Standard }
+    }
   } catch (err) {
     logger.error(err)
-    return generateDefaultError()
   }
+  return generateDefaultError()
 }
